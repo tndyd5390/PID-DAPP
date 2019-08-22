4955ca5f5a20a3f15e4633004275e97b04b9fb24
tndyd5390 <tndyd5390@naver.com>
2019-08-22 19:54:35 +0900

    기업목록으로 고침

--- routes/company.js
+++ routes/company.js
@@ -3,19 +3,34 @@ const router = express.Router();
 const fetch = require("node-fetch");
 const axios = require("axios");
 
+_nvl = (str) => {
+    return str || "";
+}
+
+_strIsEmpty = (str) => {
+	if (typeof str == "undefined" || str == null || str == "") return true;
+	else return false;
+}
+
 router.get("/", async(req, res) => {
-
+    try{
+        var response = await axios.get("http://192.168.109.132:5000/company");
+        res.render("company/companyList", {companyList: response.data});
+    }catch(err){
+        console.log(err);
+    }
 })
 
-router.get("/compReqJoinView", async(req, res) => {
-    res.render("company/companyReqJoin")
+router.get("/signup", async(req, res) => {
+    res.render("company/signup")
 })
 
-router.post("/compReqJoinProc", async(req, res) => {
+router.post("/", async(req, res) => {
     var {body: {
             companyName, 
             companyRegistrationNumber,
             companyRepresentativeName,
+            companyContactNumber,
             companyId,
             password,
             companyPostCode,
@@ -25,17 +40,18 @@ router.post("/compReqJoinProc", async(req, res) => {
     } = req;
     
     try{
-        var response = await axios.post("http://192.168.109.132:5000/company/companyJoinRequest", {
+        var response = await axios.post("http://192.168.109.132:5000/company", {
             companyName, 
             companyRegistrationNumber,
             companyRepresentativeName,
+            companyContactNumber,
             companyId,
             password,
             companyPostCode,
             address,
             addressDetail
         })
-        res.redirect("/");
+        res.render("redirect", {msg:"등록완료", url:"/"});
 
     } catch (err) {
         console.log(err);
@@ -58,7 +74,6 @@ router.get("/checkCompanyId/:companyId", async(req, res) => {
     var {params: {companyId}} = req;
     try {
         var response = await axios.get("http://192.168.109.132:5000/company/checkCompanyId/" + companyId);
-        console.log(response.data);
         res.send(response.data);
     } catch(err) {
         console.log(err);
@@ -66,14 +81,47 @@ router.get("/checkCompanyId/:companyId", async(req, res) => {
     }
 })
 
-router.get("/companyReqList", async(req, res) => {
+// router.get("/signupList", async(req, res) => {
+//     try{
+//         var response = await axios.get("http://192.168.109.132:5000/company/signupList");
+//         res.render("company/signupList", {companyReqList: response.data});
+//     }catch(err){
+//         console.log(err);
+//     }
+// })
+
+router.put("/:companyNo", async(req, res) => {
+    var {params: {companyNo}} = req;
+
+    var {body: {
+        companyName,
+        companyRegistrationNumber,
+        companyRepresentativeName,
+        companyContactNumber,
+        companyId,
+        companyAddress,
+        companyAddressDetail,
+        companyReqStatus
+    }} = req;
+
+    var companyObj = {
+        companyName,
+        companyRegistrationNumber,
+        companyRepresentativeName,
+        companyContactNumber,
+        companyId,
+        companyAddress,
+        companyAddressDetail,
+        companyReqStatus
+    };
+
     try{
-        var response = await axios.get("http://192.168.109.132:5000/company/companyReqList");
+        var response = await axios.put("http://192.168.109.132:5000/company/" + companyNo, {companyObj})
         console.log(response.data);
-    }catch(err){
+        res.send(response.data);
+    } catch(err) {
         console.log(err);
     }
-    res.render("company/companyReqList");
 })
 
 module.exports = router;
